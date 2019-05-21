2491ed04e76d7bfec06974ada8179d004a519e87
Rubén <ruben.pacheco.matas@gmail.com>
2019-05-21 14:28:56 +0200

    Beta 0.1 authService

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,48 +13,8 @@ export class LoginComponent implements OnInit {
   email;
   emailR;
   password;
-  error = 0;
-  message;
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) { }
-
-  login(email, password) {
-    this.afAuth.auth.signInWithEmailAndPassword(email, password)
-      .then((success) => {
-        this.router.navigate([`/home`]);
-      }).catch((error) =>  {
-        this.error = 1;
-        this.message = error.message;
-        console.log(error);
-      });
-  }
-
-  signin(email, password) {
-    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
-     .then((success) => {
-       const name = email.substring(0, email.lastIndexOf('@'));
-       console.log(name);
-       this.afAuth.auth.currentUser.updateProfile({
-         displayName: name
-       });
-       console.log(this.afAuth.auth.currentUser.displayName);
-       this.router.navigate([`/home`]);
-     }).catch((error) =>  {
-      this.error = 2;
-      this.message = error.message;
-      console.log(error);
-    });
-  }
-
-  resPassword(emailR) {
-    this.error = 4;
-    this.afAuth.auth.sendPasswordResetEmail(emailR).then(function() {
-      // Email sent.
-    }).catch(error => {
-      this.error = 3;
-      this.message = error.message;
-    });
-  }
+  constructor(public afAuth: AngularFireAuth, private router: Router, public authService: AuthService) { }
 
   ngOnInit() {
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,15 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class AuthService {
 
   authState = null;
+  error = 0;
+  message;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private router: Router) {
 
     // Keep the session active.
 
     this.afAuth.authState.subscribe(res => {
       this.authState = res;
-      console.log('authstate', this.authState);
     });
   }
 
@@ -32,4 +34,49 @@ export class AuthService {
   logout() {
     this.afAuth.auth.signOut();
   }
+
+  // Log In
+
+  login(email, password) {
+    this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((success) => {
+        this.router.navigate([`/home`]);
+      }).catch((error) =>  {
+        this.error = 1;
+        this.message = error.message;
+        console.log(error);
+      });
+  }
+
+  // Sign In
+
+  signin(email, password) {
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+     .then((success) => {
+       const name = email.substring(0, email.lastIndexOf('@'));
+       console.log(name);
+       this.afAuth.auth.currentUser.updateProfile({
+         displayName: name
+       });
+       console.log(this.afAuth.auth.currentUser.displayName);
+       this.router.navigate([`/home`]);
+     }).catch((error) =>  {
+      this.error = 2;
+      this.message = error.message;
+      console.log(error);
+    });
+  }
+
+  // Res Password
+
+  resPassword(emailR) {
+    this.error = 4;
+    this.afAuth.auth.sendPasswordResetEmail(emailR).then(function() {
+      // Email sent.
+    }).catch(error => {
+      this.error = 3;
+      this.message = error.message;
+    });
+  }
+
 }
